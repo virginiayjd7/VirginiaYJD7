817be7897ce934f6e7fd70af24ad42e31474919a
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2022-09-23 16:22:24 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,40 +2,21 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-       Soy egresada de la carrera profesional de ingeniería de sistemas, en la actualidad me encuentro formándome como DevOps Engineer jr y desarrollador Backend en C#, GO y Python, tengo conocimientos básicos en Frontend con vue.js y JavaScript.
-
-Tengo conocimientos básicos de contenedores de Docker y soy nueva explorando en Kubernetes.
-
-Además de conocimientos de GNU/Linux con distribuciones derivadas de CentOS y RedHat.
-
-Durante mi carrera universitaria desarrolle aplicaciones web con C#,SQL Server, MySQL y en la plataforma de .NET, .Net Core y aplicaciones de escritorio con Windows Forms.
-
-Mi meta es expandir mis conocimientos sobre Desarrollo web, DevOps ,Sistemas operativos y Microservicios . 
-
-Me fascina aprender sobre tecnología investigar ,oír podcast y leer material para fortalecer mis conocimientos, disfruto permanecer en continuo aprendizaje.
-
-En la actualidad me encuentro participando en un Bootcamp Devops y desarrollando una app dirigida al sector Agrario, practicando y reforzando en las habilidades previamente. 💙
+       En proceso de construcción 💙
     </p>
     <h3>My Skills</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="">babel</a></li>
+      <li><a href="">eslint</a></li>
     </ul>
     <h3>My Projects</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a href="*" target="_blank" rel="noopener">Core Docs</a></li>
+      
     </ul>
     <h3>My Blogs</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="*" target="_blank" rel="noopener">vue-router</a></li>
     </ul>
   </div>
 </template>
